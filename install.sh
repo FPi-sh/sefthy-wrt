184570cbb4faace7b23fd43c6b9828308944c6b1
FPi-sh <ff.passerini@gmail.com>
2025-07-31 18:08:18 +0200

    update to use uci / openwrt native config

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 version=$(cat /etc/openwrt_release | grep DISTRIB_RELEASE | cut -d"'" -f2)
-supported_versions="22.03.7 23.05.5 24.10.1"
+supported_versions="24.10.0 24.10.1 24.10.2"
 
 is_supported=0
 for supported in $supported_versions; do
@@ -10,6 +10,8 @@ for supported in $supported_versions; do
         break
     fi
 done
+
+grep "NethSecurity" /etc/openwrt_release && is_supported=1
 
 if [ $is_supported -eq 0 ]; then
     echo "WARNING: OpenWrt version $version has not been tested with SEFTHY."
@@ -28,45 +30,16 @@ if [ $is_supported -eq 0 ]; then
     esac
 fi
 
+wget https://static.sefthy.cloud/openwrt/sefthy.pub -O /tmp/sefthy.pub && 
+opkg-key add /tmp/sefthy.pub && \
+echo "src/gz sefthy https://static.sefthy.cloud/openwrt/x86_64" >> /etc/opkg/customfeeds.conf
+
 opkg update && 
-opkg install \
- bash python3-pip python3-psutil python3-netifaces \
- sipcalc curl jq sqlite3-cli vxlan ip-full iperf3 iputils-arping \
- luci-lua-runtime micrond tar wireguard-tools || exit 1
+opkg install sefthy || exit 1
 
-pip install flask flask-sqlalchemy speedtest-cli waitress requests || exit 1
-
-which unzip >/dev/null || opkg install unzip
-
-
-mkdir /opt
-for resource in "sefthy-wrt-config" "sefthy-wrt-gui" "sefthy-wrt-monitor" "sefthy-wrt-wh" "sefthy-wrt-velch"; do
-  wget https://static.sefthy.cloud/openwrt/$resource.zip -O /tmp/$resource.zip && \
-  unzip -d / /tmp/$resource.zip && \
-  rm /tmp/$resource.zip
-done
-
-chmod +x /etc/init.d/sefthy-*
-
-ln -sf /opt/sefthy-wrt-gui/uptimex /usr/sbin/uptimex
-ln -sf /opt/sefthy-wrt-gui/speedtest /usr/sbin/speedtest
-ln -sf /opt/sefthy-wrt-config/wg-quick /usr/sbin/wg-quick
-chmod +x /usr/sbin/uptimex
-chmod +x /usr/sbin/speedtest
-chmod +x /usr/sbin/wg-quick
-
-# Cron
-mkdir -p /etc/cron.d
-echo "* * * * * /opt/sefthy-wrt-config/config.sh" >> /etc/cron.d/sefthy
-ln -s /etc/cron.d/sefthy /usr/lib/micron.d/sefthy
-/etc/init.d/micrond enable
-sleep 5 && /etc/init.d/micrond restart
-
-# Fix sshx
-mkdir -p /usr/local && ln -s /usr/sbin/ /usr/local/bin
-
-# Custom Menu
-cat <<EOF > /usr/share/luci/menu.d/luci-app-sefthy.json
+grep "NethSecurity" /etc/openwrt_release >/dev/null || {
+  opkg install luci-lua-runtime
+  cat <<EOF > /usr/share/luci/menu.d/luci-app-sefthy.json
 {
   "admin/sefthy": {
     "title": "Sefthy GUI",
@@ -79,9 +52,9 @@ cat <<EOF > /usr/share/luci/menu.d/luci-app-sefthy.json
 }
 EOF
 
-mkdir -p /usr/lib/lua/luci/view/sefthy
+  mkdir -p /usr/lib/lua/luci/view/sefthy
 
-cat <<EOF > /usr/lib/lua/luci/view/sefthy/redirect.htm
+  cat <<EOF > /usr/lib/lua/luci/view/sefthy/redirect.htm
 <%
 local ip = luci.http.getenv("SERVER_NAME") or luci.http.getenv("HTTP_HOST") or "192.168.1.1"
 ip = ip:match("([^:]+)")
@@ -91,13 +64,5 @@ luci.http.redirect(sefthy_url)
 %>
 EOF
 
-rm -rf /tmp/luci* && /etc/init.d/uhttpd restart && /etc/init.d/rpcd restart
-
-# Suppress non-ECT kernel warnings
-rmmod vxlan && \
-echo "vxlan log_ecn_error=0" > /etc/modules.d/*vxlan* && \
-modprobe vxlan
-
-# Start GUI
-/etc/init.d/sefthy-wrt-gui enable
-/etc/init.d/sefthy-wrt-gui start
+  rm -rf /tmp/luci* && /etc/init.d/uhttpd restart && /etc/init.d/rpcd restart
+}
--- make.sh
+++ make.sh
@@ -7,3 +7,8 @@ for resource in "sefthy-wrt-config" "sefthy-wrt-gui" "sefthy-wrt-monitor" "sefth
   cd $base/$resource
   zip -r $base/$resource.zip ./ -x '*.git*' -x '*.DS_Store' -x '*README.md'
 done
+
+for resource in "sefthy-wrt-config" "sefthy-wrt-gui" "sefthy-wrt-monitor" "sefthy-wrt-wh" "sefthy-wrt-velch"; do
+  cd $base
+  unzip $resource.zip -d ./build/sefthy/files
+done
